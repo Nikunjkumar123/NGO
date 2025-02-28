4aa3f9ce43d97e2b6568549c7b5cb9f73f439f44
nikunj kumar <nikunjkumar2911@gmail.com>
2025-02-28 18:07:53 +0530

    fix2

--- server/server.js
+++ server/server.js
@@ -1,32 +1,41 @@
-const dotenv = require("dotenv")
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
-const express = require("express")
-const { connectDB } = require("./DB/DB")
-const cors = require("cors")
-const SignupRouter = require("./Routes/SignupRoutes")
-const verifyToken = require("./Middleware/verifyToken")
-const DonatationRouter = require("./Routes/donationRoutes")
-const router = require("./Routes/userRelationRoutes")
+const express = require("express");
+const { connectDB } = require("./DB/DB");
+const cors = require("cors");
+const SignupRouter = require("./Routes/SignupRoutes");
+const verifyToken = require("./Middleware/verifyToken");
+const DonatationRouter = require("./Routes/donationRoutes");
+const router = require("./Routes/userRelationRoutes");
 
+const app = express();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+app.use(
+  cors({
+    origin: [
+      "https://admin.saibalikavikas.com",
+      "https://saibalikavikas.com",
+      "https://www.saibalikavikas.com",
+      "http://localhost:3000",
+      "http://localhost:3001",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send(`Server Is Running At ${process.env.PORT}`)
-})
+  res.send(`Server Is Running At ${process.env.PORT}`);
+});
 
-
-app.use("/api", SignupRouter)
-app.use("/api", DonatationRouter)
-app.use("/api", router)
-
+app.use("/api", SignupRouter);
+app.use("/api", DonatationRouter);
+app.use("/api", router);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running at ${process.env.PORT}`)
-})
+  console.log(`Server is running at ${process.env.PORT}`);
+});
 
-
-connectDB()
+connectDB();
